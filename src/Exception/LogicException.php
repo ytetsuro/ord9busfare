e65bd61e303ff94e7671034b27adb9a11e30da5d
Tetsuro Yoshikawa <tetsuro.yoshikawa@seezoo.co.jp>
2018-04-05 21:51:52 +0900

    chore: Koriym.PhpSkeletonを使ってみた

---
+++ src/Exception/LogicException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the ytetsruo.ord9busfare
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace ytetsruo\ord9busfare\Exception;
+
+class LogicException extends \LogicException
+{
+}
